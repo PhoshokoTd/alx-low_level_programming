836e8f8d225819eaa611f3b7d976d8ddad2dc1b1
Phoshoko Thabang <Phoshokothabang1@gmail.com>
2022-07-04 08:13:32 -0700

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif //_MAIN_H
